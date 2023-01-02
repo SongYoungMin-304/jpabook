6151038a9ec05cebd2697451d9789fdf1cb3f6c1
82108 <dudals789@naver.com>
2023-01-02 23:11:42 +0900

    lazy-loadding 관련 내용 추가

--- src/main/java/jpashop/jpabook/api/OrderSimpleApiController.java
+++ src/main/java/jpashop/jpabook/api/OrderSimpleApiController.java
@@ -28,7 +28,7 @@ public class OrderSimpleApiController {
 
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1(){
-        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        List<Order> all = orderRepository.findAllBySong();
         for(Order order : all){
             order.getMember().getName(); //Lazy 강제 초기화
             order.getDelivery().getAddress();
